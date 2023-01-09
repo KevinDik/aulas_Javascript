8d670b1534cbf7744a4f1923086bc126b312c5c2
kevin <kevindiegodasilvasousa@gmail.com>
2023-01-09 09:28:57 -0300

    feat: Testando funções e eventos

---
+++ modulo_1/scripts.js
@@ -0,0 +1,6 @@
+function MostraDados() {
+    nome = document.getElementById("nome").value;
+    sobrenome = document.getElementById("sobrenome").value;
+    alert(`O seu nome é ${nome} ${sobrenome}`);
+};
+
