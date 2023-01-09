f7f57297bb916f095af3bc8404247ec853c98328
kevin <kevindiegodasilvasousa@gmail.com>
2023-01-09 09:50:50 -0300

    feat: Adicionado campos de data

--- modulo_1/scripts.js
+++ modulo_1/scripts.js
@@ -1,6 +1,7 @@
 function MostraDados() {
     nome = document.getElementById("nome").value;
     sobrenome = document.getElementById("sobrenome").value;
-    alert(`O seu nome é ${nome} ${sobrenome}`);
+    idade = document.getElementById("idade").value;
+    alert(`O seu nome é ${nome} ${sobrenome} e tenho ${idade} anos`);
 };
 
