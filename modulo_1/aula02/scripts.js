d96cd7c0a50939d1b6f377744d644ae80085d955
kevin <kevindiegodasilvasousa@gmail.com>
2023-01-11 09:53:11 -0300

    feat: Criado html e logica do dever de casa

---
+++ modulo_1/aula02/scripts.js
@@ -0,0 +1,9 @@
+function soma() {
+    let v1 = valor1.value;
+    let v2 = valor2.value;
+
+    document.getElementById("resultado").innerHTML = Number(v1) + Number(v2)
+}
+function subtrair() {}
+function multiplicar() {}
+function dividir() {}
