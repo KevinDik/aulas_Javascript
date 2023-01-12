ff98c013cb822597733ee740a13ffa862fd8266c
Kevin Diego <kevindiegodasilvasousa@gmail.com>
2023-01-11 23:05:24 -0300

    feat: Criado demais funcções e finalizado Html

--- modulo_1/aula02/scripts.js
+++ modulo_1/aula02/scripts.js
@@ -2,8 +2,23 @@ function soma() {
     let v1 = valor1.value;
     let v2 = valor2.value;
 
-    document.getElementById("resultado").innerHTML = Number(v1) + Number(v2)
+    document.getElementById("resultado").innerHTML = Number(v1) + Number(v2);
 }
-function subtrair() {}
-function multiplicar() {}
-function dividir() {}
+function subtrair() {
+    let v1 = valor1.value;
+    let v2 = valor2.value;
+
+    document.getElementById("resultado").innerHTML = Number(v1) - Number(v2);
+}
+function multiplicar() {
+    let v1 = valor1.value;
+    let v2 = valor2.value;
+
+    document.getElementById("resultado").innerHTML = Number(v1) * Number(v2);
+}
+function dividir() {
+    let v1 = valor1.value;
+    let v2 = valor2.value;
+
+    document.getElementById("resultado").innerHTML = Number(v1) / Number(v2);
+}
