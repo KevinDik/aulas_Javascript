623eaffc9ee457452fb934f4777d34e5ace91a9c
kevin <kevindiegodasilvasousa@gmail.com>
2023-01-18 09:01:27 -0300

    fix: Corrigindo calculos e reduzindo codigo

--- modulo_1/aula03/scripts.js
+++ modulo_1/aula03/scripts.js
@@ -17,19 +17,12 @@ function CalcularIMC() {
     
     //console.log(isNaN(parseFloat(nome.value)) && isFinite(nome.value))
     if(nomeUsuario && pesoUsuario && alturaUsuario != 0) {
-        if(calc < 18.5){
-            resultado.innerHTML = `${nomeUsuario} Você esta abaixo da media. IMC ${Math.round(calc)}`;
-        } else if(18.5 >= calc < 24.9){
-            resultado.innerHTML = `${nomeUsuario} Você esta com o peso ideal. IMC ${Math.round(calc)}`;
-        } else if(25 >= calc < 29.9){
-            resultado.innerHTML = `${nomeUsuario} Você esta com sobrepeso. IMC ${Math.round(calc)}`;
-        } else if(30 >= calc < 34.9){
-            resultado.innerHTML = `${nomeUsuario} Você esta em obesidade nivel 1. IMC ${Math.round(calc)}`;
-        } else if(35 >= calc < 39.9){
-            resultado.innerHTML = `${nomeUsuario} Você esta em obesidade nivel 2. IMC ${Math.round(calc)}`;
-        } else if(calc > 40){
-            resultado.innerHTML = `${nomeUsuario} Você esta em obesidade nivel 3. IMC ${Math.round(calc)}`;
-        }
+        (calc < 18.5) && (resultado.innerHTML = `${nomeUsuario} Você esta abaixo da media. IMC ${Math.round(calc)}`);
+        (calc >= 18.5 && calc < 24.9) && (resultado.innerHTML = `${nomeUsuario} Você esta com o peso ideal. IMC ${Math.round(calc)}`);
+        (calc >= 25 && calc < 29.9) && (resultado.innerHTML = `${nomeUsuario} Você esta com sobrepeso. IMC ${Math.round(calc)}`);
+        (calc >= 30 && calc < 34.9) && (resultado.innerHTML = `${nomeUsuario} Você esta em obesidade nivel 1. IMC ${Math.round(calc)}`);
+        (calc >= 35 && calc < 39.9) && (resultado.innerHTML = `${nomeUsuario} Você esta em obesidade nivel 2. IMC ${Math.round(calc)}`);
+        (calc > 40) && (resultado.innerHTML = `${nomeUsuario} Você esta em obesidade nivel 3. IMC ${Math.round(calc)}`);
     } else {
         alert('Atenção alguns dados não foram preenchidos')
         nome.focus();
