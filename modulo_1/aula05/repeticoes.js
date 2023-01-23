1d55319cb514a36d6618eec0ef4a66b922fc6814
kevin <kevindiegodasilvasousa@gmail.com>
2023-01-23 09:50:31 -0300

    Pratica de repetições com for e while

---
+++ modulo_1/aula05/repeticoes.js
@@ -0,0 +1,18 @@
+for (let index = 0; index < 10; index++) { 
+    console.log(index);
+}
+
+for (let vaca = 0; vaca < 10; vaca++) {
+    console.log(vaca + 1);
+}
+
+let vaca = 2;
+for (let i = 0; i < 10; ++i) {
+    console.log(vaca + 1);
+}
+
+let rato = 5;
+while (rato > 0) {
+    console.log("alguma coisa");
+    rato--;
+}
